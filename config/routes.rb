2eba78344eec7578bd7eeef499861002d8f98779
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-08 13:57:21 -0700

    Add resources for merchant discounts

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
     resources :items, except: [:show]
     get '/orders/:order_id', to: "orders#show"
     patch '/orders/:order_id', to: "orders#update"
-    resources :discounts, except: [:update, :edit, :delete]
+    resources :discounts
   end
 
   resources :merchants, except: [:delete]
