dcf8893ea74bef42d6f808f028657d80b97a6092
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-08 15:43:40 -0700

    Update controller with update action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -26,8 +26,20 @@ class Merchant::DiscountsController < Merchant::BaseController
     @discount = Discount.find(params[:id])
   end
 
+  def update
+    @discount = Discount.find(params[:format])
+    @discount.attributes = discount_params
+    if @discount.save
+      flash[:success] = 'Discount has been successfully updated!'
+      redirect_to merchant_discounts_path
+    else
+      flash[:error] = @discount.errors.full_messages.to_sentence
+      redirect_to "/merchant/discounts/#{@discount.id}/edit"
+    end
+  end
+
   private
   def discount_params
-    params.permit(:name,:percentage,:min_purchase)
+    params.require(:discount).permit(:name, :percentage, :min_purchase, :id)
   end
 end
