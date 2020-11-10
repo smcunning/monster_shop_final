1f2c416c90271c5367730014009de510dda74a7b
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-10 14:35:23 -0700

    Update expectations with integers instead of floats

--- spec/features/merchant/discounts/destroy_spec.rb
+++ spec/features/merchant/discounts/destroy_spec.rb
@@ -4,9 +4,9 @@ describe 'As a merchant' do
   before :each do
     @merchant = create(:merchant)
     @merchant_user = create(:user, role: 1, merchant_id: @merchant.id)
-    @discount_1 = Discount.create!(name: "5% Off", percentage: 0.05, min_purchase: 5, active?: true, merchant_id: @merchant.id)
-    @discount_2 = Discount.create!(name: "10% Off", percentage: 0.10, min_purchase: 10, active?: true, merchant_id: @merchant.id)
-    @discount_3 = Discount.create!(name: "25% Off", percentage: 0.25, min_purchase: 5, active?: false, merchant_id: @merchant.id)
+    @discount_1 = Discount.create!(name: "5% Off", percentage: 5, min_purchase: 5, active?: true, merchant_id: @merchant.id)
+    @discount_2 = Discount.create!(name: "10% Off", percentage: 10, min_purchase: 10, active?: true, merchant_id: @merchant.id)
+    @discount_3 = Discount.create!(name: "25% Off", percentage: 25, min_purchase: 5, active?: false, merchant_id: @merchant.id)
 
     visit login_path
     fill_in :email, with: @merchant_user.email
