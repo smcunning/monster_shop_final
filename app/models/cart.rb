7bdea28acee52dd76f7d08947ef9e09fd76dc777
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-09 23:12:44 -0700

    Add sale price method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -51,4 +51,11 @@ class Cart
     item.merchant.discounts.where("? >= min_purchase", item_count(item.id)).order(percentage: :desc).limit(1).pluck(:percentage).first
   end
 
+  def sale_price(item)
+    if apply_discount(item)
+      item.price * (100 - apply_discount(item))*0.01
+    else
+      item.price
+    end
+  end
 end
