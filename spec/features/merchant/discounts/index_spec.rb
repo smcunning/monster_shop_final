7ac0827b118a8bcd343d6848971e98d09414d079
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-11-10 13:30:09 -0700

    Add tests for discounts activation/deactivation links

--- spec/features/merchant/discounts/index_spec.rb
+++ spec/features/merchant/discounts/index_spec.rb
@@ -5,9 +5,9 @@ describe "Merchant Discounts Index Page" do
     before(:each) do
       @merchant = create(:merchant)
       @merchant_user = create(:user, role: 1, merchant_id: @merchant.id)
-      @discount_1 = Discount.create!(name: "5% Off", percentage: 0.05, min_purchase: 5, active?: true, merchant_id: @merchant.id)
-      @discount_2 = Discount.create!(name: "10% Off", percentage: 0.10, min_purchase: 10, active?: true, merchant_id: @merchant.id)
-      @discount_3 = Discount.create!(name: "25% Off", percentage: 0.25, min_purchase: 5, active?: false, merchant_id: @merchant.id)
+      @discount_1 = Discount.create!(name: "5% Off", percentage: 5, min_purchase: 5, active?: true, merchant_id: @merchant.id)
+      @discount_2 = Discount.create!(name: "10% Off", percentage: 10, min_purchase: 10, active?: true, merchant_id: @merchant.id)
+      @discount_3 = Discount.create!(name: "25% Off", percentage: 25, min_purchase: 5, active?: false, merchant_id: @merchant.id)
 
       visit login_path
       fill_in :email, with: @merchant_user.email
@@ -49,6 +49,48 @@ describe "Merchant Discounts Index Page" do
         click_link "Create Discount"
 
         expect(current_path).to eq("/merchant/discounts/new")
+      end
+    end
+
+    it 'has a link to activate an inactive discount' do
+      visit "/merchant/discounts"
+
+      within "#discount-#{@discount_3.id}" do
+        expect(page).to have_content("Discount Inactive")
+        expect(page).to_not have_link('Deactivate')
+      end
+
+      within "#discount-#{@discount_3.id}" do
+        click_on 'Activate'
+      end
+
+      expect(current_path).to eq("/merchant/discounts")
+      expect(page).to have_content('This discount is now activated.')
+
+      within "#discount-#{@discount_3.id}" do
+        expect(page).to have_content("Discount Active")
+        expect(page).to have_link('Deactivate')
+      end
+    end
+
+    it 'has a link to disactivate an active discount' do
+      visit "/merchant/discounts"
+
+      within "#discount-#{@discount_1.id}" do
+        expect(page).to have_content("Discount Active")
+        expect(page).to_not have_link('Activate')
+      end
+
+      within "#discount-#{@discount_1.id}" do
+        click_on 'Deactivate'
+      end
+
+      expect(current_path).to eq("/merchant/discounts")
+      expect(page).to have_content('This discount is now inactive.')
+
+      within "#discount-#{@discount_1.id}" do
+        expect(page).to have_content("Discount Inactive")
+        expect(page).to have_link('Activate')
       end
     end
   end
